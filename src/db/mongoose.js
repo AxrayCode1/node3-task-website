4bc04f03ec5b281b014f7b7be5fff1c0f66abe18
SHIHCHANG <axray@hotmail.com>
2020-03-23 23:40:26 +0800

    complete create task api

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,15 +5,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manger-api', {
     useCreateIndex: true
 })
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-        
-    }
-})
+
