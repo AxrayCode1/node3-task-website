6422022843f580c3c55fe558cb89ae44883d84d3
SHIHCHANG <axray@hotmail.com>
2020-05-15 23:02:42 +0800

    add README and fix sendgrid bug

---
+++ README.md
@@ -0,0 +1,7 @@
+Use express to complete users and tasks restful API
+
+1. 使用 mongoose 當作儲存資料庫
+2. 使用 sendgrid 來送 email
+3. 使用 jwt 來認證使用者
+4. 使用 supertest & jest 來做測試
+5. 使用 Postman Run restful API
--- src/emails/account.js
+++ src/emails/account.js
@@ -6,7 +6,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'axray0211@example.com',
+        from: 'axray@hotmail.com',
         subject: 'Thanks for joining in!',
         text: `Welecome to the app, ${name}. Let me know how you get along with the app.`
     })
@@ -15,7 +15,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'axray0211@example.com',
+        from: 'axray@hotmail.com',
         subject: 'Sorry to see you go!',
         text: `Groodbye, ${name}. I hope to see you back sometime soon.`
     })
